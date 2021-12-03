1b9061af9500d56c36856aaec319c321aedccbf9
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-03 22:23:30 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2021
+These are my solutions to Advent of Code 2021, delivered in my favourite format of a web API
+for no particular reason other than me wanting to play around with Ktor.
+
+All challenges are reachable on `GET http://localhost:8080/day/{day}/part/{part}`, the input
+data is located in `src/main/resources`.
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,6 +5,21 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main() {
+  val banner = """
+        _       _                 _            __    ____          _
+       / \   __| |_   _____ _ __ | |_    ___  / _|  / ___|___   __| | ___
+      / _ \ / _` \ \ / / _ \ '_ \| __|  / _ \| |_  | |   / _ \ / _` |/ _ \
+     / ___ \ (_| |\ V /  __/ | | | |_  | (_) |  _| | |__| (_) | (_| |  __/
+    /_/   \_\__,_| \_/ \___|_| |_|\__|  \___/|_|    \____\___/ \__,_|\___|
+                             ____   ___ ____  _
+                            |___ \ / _ \___ \/ |
+                              __) | | | |__) | |
+                             / __/| |_| / __/| |
+                            |_____|\___/_____|_|
+
+  """.trimIndent()
+  println(banner)
+
   embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
     configureRouting()
     configureSerialization()
