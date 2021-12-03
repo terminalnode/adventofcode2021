ef7bdf74de374bcd70ebff9288f7c7546efd26ea
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-03 22:11:04 +0100

    add solution day 1

--- src/main/kotlin/com/example/solution/day1/Day1.kt
+++ src/main/kotlin/com/example/solution/day1/Day1.kt
@@ -1,11 +1,44 @@
 package com.example.solution.day1
 
 import com.example.solution.Solution
+import java.util.*
 
-object Day1 : Solution {
-  override val day = 1
+object Day1 : Solution(1) {
+  override fun run(part: Int) =
+    when (part) {
+      1 -> partOne()
+      2 -> partTwo()
+      else -> TODO("Not yet implemented")
+    }
 
-  override fun run(part: Int): Any {
-    TODO("Not yet implemented")
+  private fun partOne(): Any {
+    val depthIterator = readLines("/day1part1.txt").map { it.toInt() }.iterator()
+    var previousValue = depthIterator.next()
+    var count = 0
+
+    depthIterator.forEachRemaining {
+      if (it > previousValue) count++
+      previousValue = it
+    }
+
+    return count
+  }
+
+  private fun partTwo(): Any {
+    val depthIterator = readLines("/day1part1.txt").map { it.toInt() }.iterator()
+    val window = LinkedList(listOf(depthIterator.next(), depthIterator.next(), depthIterator.next()))
+    var previousValue = window.sum()
+    var count = 0
+
+    depthIterator.forEachRemaining {
+      window.removeFirst()
+      window.addLast(it)
+      val newValue = window.sum()
+
+      if (newValue > previousValue) count++
+      previousValue = newValue
+    }
+
+    return count
   }
 }
