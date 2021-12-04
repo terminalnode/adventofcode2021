c61d960d462cf82dff2b0247b8227d0bd87f2238
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-04 19:36:45 +0100

    add solution day 4 part 2

--- src/main/kotlin/com/example/solution/day4/Day4.kt
+++ src/main/kotlin/com/example/solution/day4/Day4.kt
@@ -27,6 +27,23 @@ object Day4 : Solution(4) {
   }
 
   fun partTwo() : Any {
-    return ""
+    val lines = readLines("day4.txt").iterator()
+    val numbers = lines.next().split(",").map { it.toInt() }.iterator()
+    var boards = BingoBoard.generateBoards(lines)
+
+    var number = -1
+    while (numbers.hasNext() && !boards.first().checkBoard()) {
+      number = numbers.next()
+      boards.forEach { it.mark(number) }
+
+      if (boards.size > 1) {
+        // We need to play the game until the last board has won, but we don't want to filter
+        // out the last board. So we only trigger this if we have more than one board.
+        boards = boards.filter { !it.checkBoard() }
+      }
+    }
+
+    val winner = boards.first()
+    return winner.score(number)
   }
 }
