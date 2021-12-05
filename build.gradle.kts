16f0d010dcb8d647d1da560db7a0a7499eb8a993
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-05 19:18:29 +0100

    remove tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,4 @@ dependencies {
   implementation("io.ktor:ktor-gson:$ktor_version")
   implementation("io.ktor:ktor-server-netty:$ktor_version")
   implementation("ch.qos.logback:logback-classic:$logback_version")
-  testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-  testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
