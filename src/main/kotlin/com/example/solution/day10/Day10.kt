e7e47d69b29c693c49df6be21cb751102282d1be
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-10 11:41:44 +0100

    add solution day 10 part 2

--- src/main/kotlin/com/example/solution/day10/Day10.kt
+++ src/main/kotlin/com/example/solution/day10/Day10.kt
@@ -7,9 +7,12 @@ object Day10 : Solution(10, "Syntax Scoring") {
   override fun run(part: Int): String {
     return when (part) {
       1 -> partOne()
+      2 -> partTwo()
       else -> TODO("Not yet implemented")
     }
   }
+
+  private val openingBrackets = listOf('(', '[', '{', '<')
 
   private val matchingBrackets = mapOf(
     '(' to ')',
@@ -17,13 +20,17 @@ object Day10 : Solution(10, "Syntax Scoring") {
     '{' to '}',
     '<' to '>')
 
-  private val openingBrackets = listOf('(', '[', '{', '<')
-
-  private val pointMap = mapOf(
+  private val partOnePointMap = mapOf(
     ')' to 3,
     ']' to 57,
     '}' to 1197,
     '>' to 25137)
+
+  private val partTwoPointMap = mapOf(
+    ')' to 1,
+    ']' to 2,
+    '}' to 3,
+    '>' to 4)
 
   private fun partOne() : String {
     val lines = readLines("day10.txt").map { it.iterator() }
@@ -38,12 +45,48 @@ object Day10 : Solution(10, "Syntax Scoring") {
         } else {
           val expected = expectedValues.pop()
           if (char != expected) {
-            points += pointMap[char] ?: throw IllegalStateException("Missing points for char $char")
+            points += partOnePointMap[char] ?: throw IllegalStateException("Missing points for char $char")
           }
         }
       }
     }
 
     return points.toString()
+  }
+
+  private fun partTwo() : String {
+    val lines = readLines("day10.txt").map { it.iterator() }
+    val scores = mutableListOf<Long>()
+
+    lines.forEach { line ->
+      var scoreLine = true
+      val expectedValues = LinkedList<Char>()
+
+      while (line.hasNext()) {
+        val char = line.nextChar()
+
+        if (openingBrackets.contains(char)) {
+          expectedValues.addFirst(matchingBrackets[char])
+        } else {
+          val expected = expectedValues.pop()
+          if (char != expected) {
+            scoreLine = false
+            break
+          }
+        }
+      }
+
+      if (scoreLine) {
+        scores.add(scorePartTwo(expectedValues))
+      }
+    }
+
+    return scores.sorted().drop(scores.size / 2).first().toString()
+  }
+
+  private fun scorePartTwo(chars: Iterable<Char>) : Long {
+    return chars.fold(0) { acc, char ->
+      return@fold acc * 5 + partTwoPointMap[char]!!
+    }
   }
 }
