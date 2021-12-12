1834fece262515a3800b05e682f0411c33b0b2be
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-12-12 20:38:54 +0100

    generalize solution for day 12

--- src/main/kotlin/solution/day12/Day12.kt
+++ src/main/kotlin/solution/day12/Day12.kt
@@ -29,13 +29,13 @@ object Day12 : Solution(12, "Passage Pathing") {
     return PathList().also { it.add(CavePath(startNode)) }
   }
 
-  override fun partOne(): String {
+  private fun solve(enablePassTwice: Boolean = false) : String {
     var pathCount = 0
     val nodeMap = generateNodeMap("day12.txt")
     val ongoingPaths = initPathList(nodeMap)
 
     while (ongoingPaths.isNotEmpty()) {
-      val newPaths = ongoingPaths.removeFirst().branch()
+      val newPaths = ongoingPaths.removeFirst().branch(enablePassTwice)
       val unfinishedPaths = newPaths.filter { !it.isFinished() }
 
       pathCount += newPaths.size - unfinishedPaths.size
@@ -45,23 +45,14 @@ object Day12 : Solution(12, "Passage Pathing") {
     return "Number of paths: $pathCount"
   }
 
+  override fun partOne(): String {
+    return solve()
+  }
+
   override fun partTwo(): String {
     // day12-test-10.txt = 36 paths
     // day12-test-19.txt = 103 paths
     // day12-test-226.txt = 3509 paths
-
-    var pathCount = 0
-    val nodeMap = generateNodeMap("day12.txt")
-    val ongoingPaths = initPathList(nodeMap)
-
-    while (ongoingPaths.isNotEmpty()) {
-      val newPaths = ongoingPaths.removeFirst().branch(true)
-      val unfinishedPaths = newPaths.filter { !it.isFinished() }
-
-      pathCount += newPaths.size - unfinishedPaths.size
-      ongoingPaths.addAll(unfinishedPaths)
-    }
-
-    return "Number of paths: $pathCount"
+    return solve(true)
   }
 }
